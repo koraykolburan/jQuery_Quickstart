b7f09002e896bdde897c161c61c6b269c011220e
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-02-22 19:01:55 +0300

    Event Bubbling and Capturing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //on() API - is an Event Handler Function
 function toggleInfoPanel(evt){
-    $(this).siblings('.menu-item-info').toggleClass('is-visible');
+    $(this).siblings('.menu-item-info, .account').toggleClass('is-visible');
+    //$(this).siblings('.account').toggleClass('is-visible');
 }
 
 /* 
@@ -16,3 +17,13 @@ $('.menu-item > a').on("mouseover", toggleInfoPanel).on("mouseout", toggleInfoPa
 //You can provide multiple events name in the "on" call! (if you set same function)
 $('.menu-item > a').on("mouseover mouseout", toggleInfoPanel);
 
+// Event Bubbling
+$('.box-1').on("click", e => {
+    console.log('Box-1 has cliked!');
+})
+$('.box-2').on("click", e => {
+    console.log('Box-2 has cliked!');
+})
+$('.box-3').on("click", e => {
+    console.log('Box-3 has cliked!');   
+})
