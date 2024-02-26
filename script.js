4dfff24205ac6dacf06941895dee97788a4b785c
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-02-27 00:24:33 +0300

    jQuery Events & Dark Mode
    I made a dark mode using with jQuery.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //on() API - is an Event Handler Function
 function toggleInfoPanel(evt){
-    $(this).siblings('.menu-item-info, .account').toggleClass('is-visible');
+    $(this).siblings('.menu-item-info, .account').toggleClass('is-visible'); //toggleClass add or remove elements
     //$(this).siblings('.account').toggleClass('is-visible');
 }
 
@@ -43,3 +43,53 @@ const container = $("#container");
 container.on("click", (evt) => {
     evt.target.style.backgroundColor = bgChange();
 })
+
+// Event and Selector
+
+$("#btn1").click(function(){
+        $("p:odd").hide();
+    });
+
+$("#btn2").click(function(){
+    $("p:odd").show();
+})
+
+$("#p1").mouseenter(function(){
+    $("#p2").text("You entered the first paragraph! Yaaay!");
+})
+
+$("#p1").mouseleave(function(){
+    $("#p2").text("Bye bye lövely...");
+    $("#p2").css("font-style", "italic");
+    $("#p2").css("background-color", "pink");
+    $("#p2").css("border-radius", "12px");
+})
+
+
+//For Dark Mode-----------------------------------------------------------------------------------------------
+
+function darkMode(evt){
+    $("#btn3").click(function(){
+        $("*").css("background-color", "#1D2A35");
+        $("*").css("color", "white");
+        $("p").css("background-color", "none");
+        $(".box-1").css("background-color", "pink");
+        $(".box-1 > p").css("background-color", "pink");
+        $(".box-2 > p").css("background-color", "blue");
+        $(":button").css("background-color", "darkgrey");
+        $(":button").css("border-radius", "12px");
+        $(".box-1 > p").css("color", "black");
+        $(".box-2").css("background-color", "blue");
+        $(".box-3").css("background-color", "green");
+        $(".box-3 > p").css("background-color", "green");
+        $(".content3 > p").css("background-color", "grey");
+        $(".tile").css("border-color", "grey");
+        $(".menu, .menu-item, a, .menu-item-info, .cf, ul, li, .account").css("background-color", "brown");
+        $(".menu, .menu-item, a, .menu-item-info, .cf, ul, li, .account").css("border-radius", "12px");
+        $("p").css("border-radius", "12px");
+    });
+}
+
+$(document).ready("click", darkMode());
+
+//-----------------------------------------------------------------------------------------------------------------
